fb615fdc00d7f5cd154a4304f2d42b157bef3eb2
samattolebay <samattolebay@gmail.com>
2023-05-14 13:33:03 +0600

    init

---
+++ app/src/main/java/com/example/chatapp/activities/store.java
@@ -0,0 +1,33 @@
+package com.example.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.example.chatapp.R;
+
+public class store extends AppCompatActivity {
+    private ImageView next2,next3,next4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+
+        next2 = findViewById(R.id.next2);
+        next3 = findViewById(R.id.next3);
+        next4 = findViewById(R.id.next4);
+
+
+        next2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(store.this,tovar1.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/chatapp/ksk/Constants.java
@@ -0,0 +1,14 @@
+package com.example.chatapp.ksk;
+
+public class Constants {
+
+    public static final String[] productCategories = {
+            "Санузел", "Подьезд",
+            "Кухня","Басқалар"
+    };
+
+    public static final String[] productCategories1 = {
+            "Барлығы",  "Санузел", "Подьезд",
+            "Кухня","Басқалар"
+    };
+}
